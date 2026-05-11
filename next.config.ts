30372ebf35a6af5af16dea01917d846c6094caaa
Duduzcx <duduzcx@example.com>
2026-05-11 14:33:28 -0300

    fix: add disal image domain to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'qjdcexrirortchemezij.supabase.co', pathname: '/**' },
       { protocol: 'https', hostname: 'm.media-amazon.com', pathname: '/**' },
       { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'imagens.disal.com.br', pathname: '/**' },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
